20f531cee14bf8a68530d27d2b022934b65e5ec4
ykrezaihi <ykrezaihi@sfda.gov.sa>
2023-10-19 12:33:07 +0300

    setup: QueryParser

---
+++ Query/QueryParser.cs
@@ -0,0 +1,17 @@
+﻿namespace FlexibleQuery.Query
+{
+    public static class QueryParser
+    {
+        // for simplicity input sample: Level = 11
+        private static bool IsValid(string query)
+        {
+            IList<string> toknized = QueryParser.Toknize(query);
+
+            return toknized.Any();
+        }
+
+        private static IList<string> Toknize(string query) =>
+            query.Split(new char[] { ' ' });
+
+    }
+}
